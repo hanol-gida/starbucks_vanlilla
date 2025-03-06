a069f4a457f91c17d2cb68842d20642532024067
DongchanPark <patience1703@gmail.com>
2025-03-06 21:55:01 +0900

    second commit

--- js/main.js
+++ js/main.js
@@ -18,3 +18,29 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused')
   searchInputEl.setAttribute('placeholder', '')
 })
+
+const badgeEl = document.querySelector('header .badges')
+
+window.addEventListener('scroll', _.throttle(function(){
+  if (window.scrollY) {
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간) .3초의 부하
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+  gsap.to(fadeEl, 1, {
+    delay: .7 * (index +1),
+    opacity: 1,
+  });
+});
